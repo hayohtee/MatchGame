c15cea8d5869fccdb6fd3039f3fadb172e0c14ce
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-07-08 17:39:50 +0100

    Added ToggleVisibility_OnMouseDown implementation. Closes #3.

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MatchGame
 {
@@ -11,6 +12,9 @@ namespace MatchGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        TextBlock? lastTextBlockClicked;
+        bool isHidden = false;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -40,6 +44,30 @@ namespace MatchGame
                 textBlock.Text = emojiList[index];
                 emojiList.RemoveAt(index);
             }
+        }
+
+        private void ToggleVisibility_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = (TextBlock)sender;
+
+            if (!isHidden)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                isHidden = true;
+            }
+            else if (lastTextBlockClicked.Text == textBlock.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                isHidden = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                isHidden = false;
+            }
+
         }
     }
 }
